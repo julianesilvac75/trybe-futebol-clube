ec3b197a0c41f916df6104927949f4728ff1a622
Juliane Cardoso <julianesilvac75@gmail.com>
2022-08-18 19:59:06 -0300

    chore: create migration for matches table

---
+++ app/backend/src/database/migrations/20220818224950-create-matches-table.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      homeTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        // primaryKey: true,
+      },
+      homeTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        // primaryKey: true,
+      },
+      awayTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        field: 'in_progress',
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
