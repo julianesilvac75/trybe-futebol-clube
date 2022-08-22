ebc7709461705422a441b93ad284f738a010a9b4
Juliane Cardoso <julianesilvac75@gmail.com>
2022-08-22 18:15:31 -0300

    fix: first functional version of login validation

--- app/backend/src/middlewares/LoginValidate.ts
+++ app/backend/src/middlewares/LoginValidate.ts
@@ -1,42 +1,90 @@
+/* eslint-disable class-methods-use-this */
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as Joi from 'joi';
 
-class LoginValidate {
-  static error(error: Joi.ValidationErrorItem): { code: number, name: string, message: string } {
-    if (error.type.includes('required')) {
-      return {
-        code: StatusCodes.BAD_REQUEST,
-        name: 'Bad Request',
-        message: error.message,
-      };
-    }
-
+const errorMessage = (_error: Joi.ValidationErrorItem): {
+  code: number,
+  name: string,
+  message: string } => {
+  if (_error?.type.includes('required')) {
     return {
-      code: StatusCodes.UNPROCESSABLE_ENTITY,
-      name: 'Unprocessable Entity',
-      message: error.message,
+      code: StatusCodes.BAD_REQUEST,
+      name: 'Bad Request',
+      message: _error.message,
     };
   }
 
-  static validate(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
+  return {
+    code: StatusCodes.UNPROCESSABLE_ENTITY,
+    name: 'Unprocessable Entity',
+    message: _error?.message ?? '',
+  };
+};
 
-    const required = Joi.object({
-      email: Joi.string().email().required(),
-      password: Joi.string().min(7).required(),
-    });
+const loginValidate = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
 
-    const validation = required.validate({ email, password });
+  const required = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(7).required(),
+  });
 
-    if (validation.error) {
-      const error = this.error(validation.error.details[0]);
+  const validation = required.validate({ email, password });
+  // console.log(validation.error?.details[0]);
 
-      return res.status(error.code).json({ message: error.message });
-    }
+  if (validation.error) {
+    const error = errorMessage(validation.error?.details[0]);
+    console.log(error);
 
-    next();
+    return res.status(error.code).json(error.message);
   }
-}
 
-export default LoginValidate;
+  next();
+};
+
+export default loginValidate;
+
+// class LoginValidate {
+//   private error(error: Joi.ValidationErrorItem | undefined): { code: number,
+//     name: string, message: string } {
+//     if (error?.type.includes('required')) {
+//       return {
+//         code: StatusCodes.BAD_REQUEST,
+//         name: 'Bad Request',
+//         message: error.message,
+//       };
+//     }
+
+//     return {
+//       code: StatusCodes.UNPROCESSABLE_ENTITY,
+//       name: 'Unprocessable Entity',
+//       message: error?.message ?? '',
+//     };
+//   }
+
+//   static validate(req: Request, _res: Response, _next: NextFunction) {
+//     const { email, password } = req.body;
+
+//     const required = Joi.object({
+//       email: Joi.string().email().required(),
+//       password: Joi.string().min(7).required(),
+//     });
+
+//     const validation = required.validate({ email, password });
+//     console.log(validation.error?.details[0]);
+
+//     console.log(this.error(validation.error?.details[0]));
+
+//     // if (validation.error) {
+//     //   const error = this.error(validation.error.details[0]);
+//     //   console.log(error);
+
+//     //   // return res.status(error.code).json({ message: error.message });
+//     // }
+
+//     // next();
+//   }
+// }
+
+// export default LoginValidate;
--- app/backend/src/routers/UserRouter.ts
+++ app/backend/src/routers/UserRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import loginValidate from '../middlewares/LoginValidate';
 
 const UserRouter = Router();
 
-UserRouter.post('/', UserController.login);
+UserRouter.post('/', loginValidate, UserController.login);
 
 export default UserRouter;
