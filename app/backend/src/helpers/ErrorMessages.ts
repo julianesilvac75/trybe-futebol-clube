fe75997aa172e2ca800ac37590ab70384f7b76c4
Juliane Cardoso <julianesilvac75@gmail.com>
2022-08-22 18:22:26 -0300

    fix: right version of login validator

---
+++ app/backend/src/helpers/ErrorMessages.ts
@@ -0,0 +1,6 @@
+enum ErrorMessages {
+  required = 'All fields must be filled',
+  incorrectField = 'Incorrect email or password',
+}
+
+export default ErrorMessages;
--- app/backend/src/middlewares/LoginValidate.ts
+++ app/backend/src/middlewares/LoginValidate.ts
@@ -1,24 +1,24 @@
-/* eslint-disable class-methods-use-this */
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as Joi from 'joi';
+import ErrorMessages from '../helpers/ErrorMessages';
 
-const errorMessage = (_error: Joi.ValidationErrorItem): {
+const errorMessage = (error: Joi.ValidationErrorItem): {
   code: number,
   name: string,
   message: string } => {
-  if (_error?.type.includes('required')) {
+  if (error?.type.includes('required')) {
     return {
       code: StatusCodes.BAD_REQUEST,
       name: 'Bad Request',
-      message: _error.message,
+      message: ErrorMessages.required,
     };
   }
 
   return {
     code: StatusCodes.UNPROCESSABLE_ENTITY,
     name: 'Unprocessable Entity',
-    message: _error?.message ?? '',
+    message: ErrorMessages.incorrectField,
   };
 };
 
@@ -31,60 +31,14 @@ const loginValidate = (req: Request, res: Response, next: NextFunction) => {
   });
 
   const validation = required.validate({ email, password });
-  // console.log(validation.error?.details[0]);
 
   if (validation.error) {
-    const error = errorMessage(validation.error?.details[0]);
-    console.log(error);
+    const { code, message } = errorMessage(validation.error?.details[0]);
 
-    return res.status(error.code).json(error.message);
+    return res.status(code).json({ message });
   }
 
   next();
 };
 
 export default loginValidate;
-
-// class LoginValidate {
-//   private error(error: Joi.ValidationErrorItem | undefined): { code: number,
-//     name: string, message: string } {
-//     if (error?.type.includes('required')) {
-//       return {
-//         code: StatusCodes.BAD_REQUEST,
-//         name: 'Bad Request',
-//         message: error.message,
-//       };
-//     }
-
-//     return {
-//       code: StatusCodes.UNPROCESSABLE_ENTITY,
-//       name: 'Unprocessable Entity',
-//       message: error?.message ?? '',
-//     };
-//   }
-
-//   static validate(req: Request, _res: Response, _next: NextFunction) {
-//     const { email, password } = req.body;
-
-//     const required = Joi.object({
-//       email: Joi.string().email().required(),
-//       password: Joi.string().min(7).required(),
-//     });
-
-//     const validation = required.validate({ email, password });
-//     console.log(validation.error?.details[0]);
-
-//     console.log(this.error(validation.error?.details[0]));
-
-//     // if (validation.error) {
-//     //   const error = this.error(validation.error.details[0]);
-//     //   console.log(error);
-
-//     //   // return res.status(error.code).json({ message: error.message });
-//     // }
-
-//     // next();
-//   }
-// }
-
-// export default LoginValidate;
