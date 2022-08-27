5fcb4f853bdc33d2141ed229e3381083e2cdb9c7
Juliane <julianesilvac75@gmail.com>
2022-08-27 23:17:59 +0100

    feat: added findAll method for /teams route

--- app/backend/src/routers/TeamRouter.ts
+++ app/backend/src/routers/TeamRouter.ts
@@ -3,6 +3,7 @@ import TeamController from '../controllers/TeamController';
 
 const TeamRouter = Router();
 
+TeamRouter.get('/', TeamController.findAll);
 TeamRouter.get('/:id', TeamController.findById);
 
 export default TeamRouter;
