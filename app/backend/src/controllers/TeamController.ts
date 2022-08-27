65504f922a76648f0279b5222f041252e5ebe6e2
Juliane <julianesilvac75@gmail.com>
2022-08-27 22:53:27 +0100

    feat: created controller layer for /teams route

---
+++ app/backend/src/controllers/TeamController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ErrorMessages from '../helpers/ErrorMessages';
+import CustomError from '../helpers/CustomError';
+import TeamService from '../services/TeamService';
+
+class TeamController {
+  static async findById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const team = await TeamService.findById(parseInt(id, 10));
+
+      return res.status(StatusCodes.OK).json(team);
+    } catch (e) {
+      console.log(e);
+      throw new CustomError(StatusCodes.INTERNAL_SERVER_ERROR, ErrorMessages.somethingWentWrong);
+    }
+  }
+}
+
+export default TeamController;
