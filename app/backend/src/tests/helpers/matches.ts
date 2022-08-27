2282b89bc841f480dfd79714d115e91aea8d1975
Juliane <julianesilvac75@gmail.com>
2022-08-28 00:49:38 +0100

    tests: in progress matches on /matches route

--- app/backend/src/tests/helpers/matches.ts
+++ app/backend/src/tests/helpers/matches.ts
@@ -14,3 +14,18 @@ export const matchMock: IMatch = {
     teamName: 'Grêmio'
   }
 };
+
+export const inProgressMatchMock: IMatch = {
+  id: 1,
+  homeTeam: 16,
+  homeTeamGoals: 1,
+  awayTeam: 8,
+  awayTeamGoals: 1,
+  inProgress: true,
+  teamHome: {
+    teamName: 'São Paulo'
+  },
+  teamAway: {
+    teamName: 'Grêmio'
+  }
+}
--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -9,7 +9,7 @@ const { expect } = chai;
 
 import { app } from '../app';
 import Match from '../database/models/MatchModel';
-import { matchMock } from './helpers/matches';
+import { matchMock, inProgressMatchMock } from './helpers/matches';
 import { StatusCodes } from 'http-status-codes';
 
 describe('On the /matches route', () => {
@@ -34,6 +34,34 @@ describe('On the /matches route', () => {
         .get('/matches');
 
       expect(response.body).to.be.deep.equal([matchMock]);
+    });
+  });
+
+  describe('when searched for in progress matches', () => { 
+    beforeEach(() => {
+      sinon.stub(Match, 'findAll').resolves([inProgressMatchMock as Match]);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return status 200', async () => {
+      const response = await chai.request(app)
+        .get('matches')
+        .query({ inProgress: true });
+      
+      expect(response.status).to.be.equal(StatusCodes.OK);
+    });
+
+    it('should return only in progress matches', async () => {
+      const response = await chai.request(app)
+        .get('matches')
+        .query({ inProgress: true });
+      
+      expect(response.body).to.be.deep.equal([inProgressMatchMock]);
+      expect(response.body[0]).to.have.property('inProgress');
+      expect(response.body[0].inProgress).to.be.equal(true);
     });
   });
 })
