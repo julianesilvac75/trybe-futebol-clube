5fe0b5d7c2bb26a00c542551145f36b621adee35
Juliane <julianesilvac75@gmail.com>
2022-08-28 00:54:54 +0100

    feat: created findByStatus function for /matches route on service layer

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -20,6 +20,22 @@ class MatchService {
       throw new CustomError(StatusCodes.INTERNAL_SERVER_ERROR, ErrorMessages.somethingWentWrong);
     }
   }
+
+  static async findByStatus(status: boolean): Promise<IMatch[]> {
+    try {
+      const matches = await Match.findAll({
+        where: { inProgress: status },
+        include: [
+          { model: Team, as: 'teamHome', attributes: ['teamName'] },
+          { model: Team, as: 'teamAway', attributes: ['teamName'] },
+        ] });
+
+      return matches as IMatch[];
+    } catch (e) {
+      console.log(e);
+      throw new CustomError(StatusCodes.INTERNAL_SERVER_ERROR, ErrorMessages.somethingWentWrong);
+    }
+  }
 }
 
 export default MatchService;
