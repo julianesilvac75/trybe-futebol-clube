72d4e57b91a954edefce57b6ec25caff10f38e84
Juliane <julianesilvac75@gmail.com>
2022-08-28 00:27:45 +0100

    feat: created findAll function for /matches route on controllers layer

---
+++ app/backend/src/controllers/MatchController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ErrorMessages from '../helpers/ErrorMessages';
+import CustomError from '../helpers/CustomError';
+import MatchService from '../services/MatchService';
+
+class MatchController {
+  static async findAll(req: Request, res: Response) {
+    try {
+      const matches = await MatchService.findAll();
+
+      if (!matches) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: ErrorMessages.noMatchesFound });
+      }
+
+      return res.status(StatusCodes.OK).json(matches);
+    } catch (e) {
+      console.log(e);
+      throw new CustomError(StatusCodes.INTERNAL_SERVER_ERROR, ErrorMessages.somethingWentWrong);
+    }
+  }
+}
+
+export default MatchController;
