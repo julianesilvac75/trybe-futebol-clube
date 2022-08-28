a0e9949214946dbd719de18e2836ffc1b1e96431
Juliane <julianesilvac75@gmail.com>
2022-08-28 02:09:43 +0100

    feat: created create function for /matches on services layer

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -4,6 +4,8 @@ import CustomError from '../helpers/CustomError';
 import IMatch from '../interfaces/IMatch';
 import Match from '../database/models/MatchModel';
 import Team from '../database/models/TeamModel';
+import INewMatch from '../interfaces/INewMatch';
+import ICreatedMatch from '../interfaces/ICreatedMatch';
 
 class MatchService {
   static async findAll(status: boolean | null = null): Promise<IMatch[]> {
@@ -37,6 +39,20 @@ class MatchService {
         ] });
 
       return matches as IMatch[];
+    } catch (e) {
+      console.log(e);
+      throw new CustomError(StatusCodes.INTERNAL_SERVER_ERROR, ErrorMessages.somethingWentWrong);
+    }
+  }
+
+  static async create(payload: INewMatch): Promise<ICreatedMatch> {
+    try {
+      const newMatch = await Match.create({
+        ...payload,
+        inProgress: true,
+      });
+
+      return newMatch;
     } catch (e) {
       console.log(e);
       throw new CustomError(StatusCodes.INTERNAL_SERVER_ERROR, ErrorMessages.somethingWentWrong);
