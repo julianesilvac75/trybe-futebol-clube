cb8704af65ecb1e5456fec72cf398444e467b1c3
Juliane <julianesilvac75@gmail.com>
2022-08-28 02:17:12 +0100

    feat: create POST /matches route

--- app/backend/src/routers/MatchRouter.ts
+++ app/backend/src/routers/MatchRouter.ts
@@ -4,5 +4,6 @@ import MatchController from '../controllers/MatchController';
 const MatchRouter = Router();
 
 MatchRouter.get('/', MatchController.findAll);
+MatchRouter.post('/', MatchController.create);
 
 export default MatchRouter;
